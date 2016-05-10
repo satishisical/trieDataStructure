dba52e9f09543baabf4bccd38399467cce1b6216
satish <satya.bbd08@gmail.com>
2016-05-10 11:38:09 +0530

    delete make file

--- make.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-gcc -c -I../include/ ../app/cs1419-assign9-prog1.c -o cs1419-assign9-prog1.o
-gcc -c -I../include/ ../src/cs1419-assign9-src-prog1.c -o cs1419-assign9-src-prog1.o
-gcc cs1419-assign9-src-prog1.o cs1419-assign9-prog1.o -o prog
